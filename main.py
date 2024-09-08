b28a474855a8d6b3b44c25a828e08a761d74a3c3
dpfls0922 <dpfls0922@naver.com>
2024-09-08 22:15:13 +0900

    feat: post api 작성하기

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+@app.post("/items")
+def create_item(item: Item):
+    return {"item_name": item.name, "item_price": item.price}
+
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
